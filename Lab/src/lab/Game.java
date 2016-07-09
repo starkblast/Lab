36917c03b85c357510dd960c3774881e861fb944
starkblast <chazzykaku@gmail.com>
2016-07-09 21:53:04 +0300

    the grid!!!!

--- Lab/src/lab/Game.java
+++ Lab/src/lab/Game.java
@@ -9,47 +9,34 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Game extends JPanel{
-	
-	
+	private int i;
+	private int j;
+	private String player1 = "#330000";
+	private String player2 = "#003300";
+			
 	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(Color.RED);	
-
-		g2d.fillRect(50, 0, 30, 30);
-		g2d.drawRect(50, 50, 30, 30);
+		g2d.setColor(Color.BLACK);	
+		g2d.fillRect(0, 120, 376, 376);
+		g2d.setColor(Color.decode(player1));
+		g2d.fillRect(0, 0, 376, 120);
+		g2d.setColor(Color.decode(player2));
+		g2d.fillRect(0, 496, 376, 120);
+		g2d.setColor(Color.GRAY);
+		for (i=0; i<9 ;i++) {
+			for (j=0; j<9 ;j++) {
+				g2d.fillRect(j*40 + j*2, 120 + i*40 + i*2, 40, 40);
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Labyrinth cancer game");
 		frame.add(new Game());
-		frame.setSize(360, 600);
+		frame.setSize(376, 616);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setBackground(Color.WHITE);
 	
-}
-}
-
-/*@SuppressWarnings("serial")
-public class Game extends JPanel {
-
-	@Override
-	public void paint(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(Color.RED);
-		g2d.fillOval(0, 0, 30, 30);
-		g2d.drawOval(0, 50, 30, 30);		
-		g2d.fillRect(50, 0, 30, 30);
-		g2d.drawRect(50, 50, 30, 30);
-
-		g2d.draw(new Ellipse2D.Double(0, 100, 30, 30));
 	}
-	
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Mini Tennis");
-		frame.add(new Game());
-		frame.setSize(300, 300);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-}*/
+}
