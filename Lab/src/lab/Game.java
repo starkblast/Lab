88c65cc78676ec80e6231eeff007d5c2ee6fc960
starkblast <chazzykaku@gmail.com>
2016-07-09 20:43:51 +0300

    changed message

--- Lab/src/lab/Game.java
+++ Lab/src/lab/Game.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class Game {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Mini Tennis");
+		JFrame frame = new JFrame("Labyrinth cancer game");
 		frame.setSize(300, 300);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
