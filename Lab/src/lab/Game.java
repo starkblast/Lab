2d863d0561ce94ba623c4c1bbaefe9202f2eb113
starkblast <chazzykaku@gmail.com>
2016-07-09 20:46:44 +0300

    hcgaend size

--- Lab/src/lab/Game.java
+++ Lab/src/lab/Game.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Game {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Labyrinth cancer game");
-		frame.setSize(300, 300);
+		frame.setSize(350, 600);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
